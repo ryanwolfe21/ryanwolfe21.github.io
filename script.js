7e6e8754b21bd378e175143951000a6f451673d9
ryanwolfe21 <69769079+ryanwolfe21@users.noreply.github.com>
2020-09-11 00:07:12 -0700

    initial commit

--- script.js
+++ script.js
@@ -17,5 +17,9 @@ document.getElementById("save").addEventListener("click", function() {
     window.location.reload()
   });
 
+  document.getElementById("delete").addEventListener('click', function(){
+    localStorage.removeItem("favoriteMovies")
+  })
+
 
   renderList(); 
