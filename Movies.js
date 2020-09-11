1c594f4d56cd4d5eac7f66386879c8809ccc5aa0
ryanwolfe21 <69769079+ryanwolfe21@users.noreply.github.com>
2020-09-10 23:15:55 -0700

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -1,25 +1,21 @@
+//Add Local Storage and Event Listeners
+const movies = JSON.parse(localStorage.getItem("favoriteMovies")) || []
 
-//Push the text from the text form to the list below
-const moviesArr = localStorage.getItem("favoriteMovies")
-document.getElementById("save").onclick = function() {
-  var text = document.getElementById("favoriteMovies").value;
-  var li = document.createElement("li");
-  li.textContent = text;
-  document.getElementById("movieList").appendChild(li);
+function renderList () {
+       movies.forEach(arr1 => {
+           var li = document.createElement("li");
+        li.textContent = arr1;
+        document.getElementById("movieList").appendChild(li);
+       })
+      
 }
 
 document.getElementById("save").addEventListener("click", function() {
-  alert("Movie Added!");
-  }, false);
+    var favoriteMovies = document.getElementById("favoriteMovies");
+    movies.push(favoriteMovies.value)
+    localStorage.setItem("favoriteMovies",JSON.stringify(movies));
+    window.location.reload()
+  });
 
-//Local Storage for Form to enter your favorite movies
-if (window.localStorage) {
-  var favoriteMovies = document.getElementById("favoriteMovies");
 
-  favoriteMovies.value = localStorage.getItem("favoriteMovies");
-
-  favoriteMovies.addEventListener("input", function () {
-      localStorage.setItem("favoriteMovies", favoriteMovies.value);
-    },
-    false);
-}
+  renderList(); 
